4ab1e77a1f4232d8da1c989a83678d6f3274dc2d
qc <you@example.com>
2017-07-03 10:59:47 +0800

    init

--- src/sync/waitgroup.go
+++ src/sync/waitgroup.go
@@ -1,6 +1,8 @@
 // Copyright 2011 The Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
+//
+// [[[2-over]]] 2017-7-3 10:52:03
 
 package sync
 
